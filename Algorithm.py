19ffbcc3e350b4c727ac173b2da8078cd1469e67
memreyagci <47505856+memreyagci@users.noreply.github.com>
2019-09-13 05:58:58 +0200

    Added functionality for finding common words

--- Algorithm.py
+++ Algorithm.py
@@ -5,14 +5,22 @@ from bs4 import BeautifulSoup
 from nltk.tokenize import sent_tokenize, word_tokenize
 
 articleUser = Article(input("Link: "))
-articleUser.currentWebsiteIs()
-articleUser.cleanText()
-articleUser.getDict()
+articleUser.run()
 articleUser.getRelated()
-
 
 articles = []
 
 for x in range(len(articleUser.compareTo)):
     articles.append(Article(articleUser.compareTo[x]))
  
+for article in articles:
+    article.run()
+
+commonWords = []
+
+for article in articles:
+    for word in article.cleanText():
+        if word in articleUser.cleanText():
+            commonWords.append(word)
+
+print(commonWords)
